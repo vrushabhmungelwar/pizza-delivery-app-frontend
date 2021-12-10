236638c0a5c6e82aedab18930ba48925ab25ffa0
vrushabhmungelwar <vrushabhmungelwar10@gmail.com>
2021-12-10 10:27:58 +0530

    fixed errors

--- src/pizza.js
+++ src/pizza.js
@@ -2,6 +2,21 @@ export function SuccessfulLogIn() {
   return (
     <div>
       <h1>login successful</h1>
+
+      <div>
+<h2>Maggareta</h2>
+<h2>Bacon</h2>
+<h2>Chili pepper</h2>
+<h2>Mushrooms</h2>
+<h2>Vegetarian</h2>
+<h2>Shrimp</h2>
+<h2>salami</h2>
+
+
+      </div>
     </div>
+
+
+
   );
 }
--- src/userLogIn.js
+++ src/userLogIn.js
@@ -1,6 +1,6 @@
 import { useFormik } from "formik";
 import * as yup from "yup";
-// import { API_URL } from "./global-constants";
+import { API_URL } from "./global-constants";
 import { useHistory } from "react-router-dom";
 
 const formValidationSchema = yup.object({
@@ -29,18 +29,17 @@ export function UserLogIn() {
     });
 
   const checkCredentials = async (values) => {
-    const response = await fetch(
-      `https://pizza-by-vrushabh.herokuapp.com/user/signin`,
-      {
-        method: "POST",
-        body: JSON.stringify({email: values.email, password: values.password}),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await fetch(`${API_URL}/user/signin`, {
+      method: "POST",
+      body: JSON.stringify({
+        email: values.email,
+        password: values.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     const json = await response.json();
-    console.log(json);
     if (json.success) {
       history.push("/pizza");
     }
@@ -50,26 +49,30 @@ export function UserLogIn() {
     <div>
       <h2>hello user</h2>
       <form onSubmit={handleSubmit}>
-        <input
-          id="email"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          type="email"
-          placeholder="Enter your email"
-        />
-        {errors.email && touched.email && errors.email}
-        <input
-          id="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          type="password"
-          placeholder="Enter your password"
-        />
-        {errors.password && touched.password ? errors.password : ""}
+        <div className="email-input">
+          <input
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="email"
+            placeholder="Enter your email"
+          />
+          <div>{errors.email && touched.email && errors.email}</div>
+        </div>
+        <div>
+          <input
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="password"
+            placeholder="Enter your password"
+          />
+          {errors.password && touched.password ? errors.password : ""}
+        </div>
         <button type="submit">Login</button>
       </form>
     </div>
