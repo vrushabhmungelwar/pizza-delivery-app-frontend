0c89aaf980162c267bcb398d0b4d22f60fd2fa61
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2022-02-23 18:23:57 +0530

    made some changes

--- src/components/AdminLogIn.js
+++ src/components/AdminLogIn.js
@@ -1,10 +1,20 @@
+import {
+  Box,
+  TextField,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import { API_URL } from "../helpers/global-constants";
 import { useHistory } from "react-router-dom";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import copy from "copy-to-clipboard";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const formValidationSchema = yup.object({
   email: yup
@@ -19,6 +29,7 @@ const formValidationSchema = yup.object({
 });
 
 export function AdminLogIn() {
+  const theme = createTheme();
   const history = useHistory();
 
   const { handleSubmit, handleChange, values, handleBlur, errors, touched } =
@@ -26,7 +37,6 @@ export function AdminLogIn() {
       initialValues: { email: "", password: "" },
       validationSchema: formValidationSchema,
       onSubmit: (values) => {
-        // console.log("onSubmit", values);
         checkCredentials(values);
       },
     });
@@ -43,57 +53,104 @@ export function AdminLogIn() {
       },
     });
     const json = await response.json();
-    // console.log(json);
     if (json.success) {
       history.push("/adminDashboard");
     } else {
       alert("Invalid Credentials");
     }
   };
+  const copyToClipboard = async (text) => {
+    copy(text);
+  };
   return (
-    <div className="signin-container">
-      <Box
-        sx={{
-          width: 300,
-          height: 220,
-          backgroundColor: "primary",
-        }}
-      >
-        <div>
-          <h2>Admin</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="input-container">
-              <TextField
-                id="email"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                label="email"
-                error={errors.email && touched.email}
-                helperText={errors.email && touched.email && errors.email}
-                variant="standard"
-              />
+    <div>
+      <div className="demo">
+        <h3>Demo Credentials</h3>
+        <h5>
+          Email: vrushabh@gmail.com
+          <IconButton
+            onClick={() => {
+              const text = "vrushabh@gmail.com";
+              copyToClipboard(text);
+            }}
+          >
+            <ContentCopyIcon fontSize="small" />
+          </IconButton>
+        </h5>
 
-              <TextField
-                id="password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                label="password"
-                type="password"
-                error={errors.password && touched.password}
-                helperText={
-                  errors.password && touched.password && errors.password
-                }
-                variant="standard"
-              />
-            </div>
-            <Button type="submit">Login</Button>
-          </form>
-        </div>
-      </Box>
+        <h5>
+          Password: vrushabh123
+          <IconButton
+            onClick={() => {
+              const text = "vrushabh123";
+              copyToClipboard(text);
+            }}
+          >
+            <ContentCopyIcon fontSize="small" />
+          </IconButton>
+        </h5>
+      </div>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 10,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <Box sx={{ mt: 3 }}>
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      id="email"
+                      name="email"
+                      fullWidth
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      label="Email"
+                      type="email"
+                      error={errors.email && touched.email}
+                      helperText={errors.email && touched.email && errors.email}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      id="password"
+                      name="password"
+                      fullWidth
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      label="Password"
+                      type="password"
+                      error={errors.password && touched.password}
+                      helperText={
+                        errors.password && touched.password && errors.password
+                      }
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Login
+                </Button>
+              </form>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
